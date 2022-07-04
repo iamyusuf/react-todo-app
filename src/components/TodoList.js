9f3389afc9abce1dc3e99936f498eaa3adab2692
Md Yousuf Ali <yousuf3usb@gmai.com>
2022-07-04 12:01:55 +0600

    basic UI design

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,29 @@
-export default function TodoList() {
+import PropTypes from 'prop-types';
+import styled from "styled-components";
 
+const TodoItem = styled.div`
+	border: 1px solid black;
+	font-size: 1.6em;
+`
+
+export default function TodoList({ todos }) {
+	if (!todos.length) {
+		return <h1>You don't have any todos left!</h1>
+	}
+	
+	return (
+		<div>
+			{
+				todos.map(todo => (
+					<TodoItem>
+						{todo.task}
+					</TodoItem>
+				))
+			}
+		</div>
+	)
 }
 
 TodoList.propTypes = {
-	// todos: Prop
+	todos: PropTypes.array.isRequired
 };
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,4 +1,52 @@
+import styled from "styled-components";
+import TodoList from "./TodoList";
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
+const TodoCard = styled.div`
+	display: flex;
+	flex-direction: column;
+  text-align: center;
+`
+
+const TodoCardHeading = styled.div`
+  background-color: rgba(29, 194, 35, 0.73);
+	color: white;
+  margin-bottom: 1em;
+`
+
+const AddTodoButton = styled.button`
+	font-size: 1.5em;
+	width: 20%;
+`
+
+const TodoButtonArea = styled.div`
+	margin-bottom: 1em;
+`
+
+const defaultTodos = [{
+	id: nanoid(),
+	task: "Pay the bill",
+	done: false
+}]
+
+
 export default function Todos() {
-	return <h1>Todos</h1>
+	
+	const [todos, setTodos] = useState(defaultTodos);
+	
+	return <TodoCard>
+		<TodoCardHeading>
+			<h1>Todos</h1>
+		</TodoCardHeading>
+		
+		<TodoButtonArea>
+			<AddTodoButton>
+				Add New Todo
+			</AddTodoButton>
+		</TodoButtonArea>
+		
+		<TodoList todos={todos} />
+	</TodoCard>
 }
 
